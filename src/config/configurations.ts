cdd0ec560d8dfb62da21de51fd83519d4832a1b4
longvb <vuongbaolong48@gmail.com>
2022-09-05 13:36:40 +0000

    update port

--- src/config/configurations.ts
+++ src/config/configurations.ts
@@ -1,5 +1,5 @@
 export default () => ({
-  port: parseInt(process.env.PORT, 10) || 3000,
+  port: parseInt(process.env.PORT, 10) || 8080,
   host: "0.0.0.0",
   database: {
     host: process.env.DATABASE_HOST,
