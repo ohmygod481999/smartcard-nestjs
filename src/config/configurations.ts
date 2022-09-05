9fffcdd16ee5c09ac14745666d78ac6a2b77bd53
longvb <vuongbaolong48@gmail.com>
2022-09-05 11:00:46 +0000

    add host

--- src/config/configurations.ts
+++ src/config/configurations.ts
@@ -1,5 +1,6 @@
 export default () => ({
   port: parseInt(process.env.PORT, 10) || 3000,
+  host: "0.0.0.0",
   database: {
     host: process.env.DATABASE_HOST,
     port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
